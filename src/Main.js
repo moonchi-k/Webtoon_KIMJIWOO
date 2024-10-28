77da6bc7ebd356d576590be21b66ef33f932b103
moonchi-k <jiwoo648@gmail.com>
2024-10-28 13:07:51 +0900

    final

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Header from "./ components/Header";
 import Sub from "./pages/Sub";
 import styled from "styled-components";
+import PageNotFound from "./pages/PageNotFound";
 
 const Wrap = styled.div`
   width: 100%;
@@ -16,7 +17,7 @@ const Main = () => {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/sub/:id" element={<Sub />}></Route>
-          {/* <Route path="/*" element={<PageNotFound />}></Route> */}
+          <Route path="/*" element={<PageNotFound />}></Route>
         </Routes>
       </HashRouter>
     </Wrap>
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,5 @@
+const PageNotFound = () => {
+  return <div>페이지가 없습니다.</div>;
+};
+
+export default PageNotFound;
